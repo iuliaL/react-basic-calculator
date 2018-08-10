23987cebb003d0c5fcbf05a826f8636bc87dc3a0
iulia <iuliamarialungu@yahoo.com>
2018-08-10 23:43:35 +0300

    added keydown behavior

--- app/App.js
+++ app/App.js
@@ -2,10 +2,9 @@
 
 // 3rd party
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 import ReactFontIt from 'react-font-it';
 import math from 'mathjs';
-
+import keydown from 'react-keydown';
 
 // Components
 import Display from './components/Display'
@@ -21,12 +20,26 @@ const fontConfig = {
 	google: ['Work Sans:200']
 };
 
+@keydown
 class App extends Component {
 
 	state = { queue: [] };
 
-	handleClick = (e) => {
-		const value = e.target.getAttribute('data-value');
+	componentWillReceiveProps(nextProps) {
+		const { keydown: { event } } = nextProps;
+		if (event) {
+			const acceptedKeys = ['.', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '-', '*', '/', '=', 'Enter', 'Backspace'];
+			const pressedKey = event.key;
+			const keyAccepted = acceptedKeys.indexOf(pressedKey) > -1;
+			if (keyAccepted) {
+				const key = (pressedKey === 'Enter' && '=') || (pressedKey === 'Backspace' && 'clear') || pressedKey;
+				this.setState({ ...this.state, pressedKey: key });
+				this.handleValue(key);
+			}
+		}
+	}
+
+	handleValue = (value) => {
 		switch (value) {
 			case 'clear':
 				this.setState({
@@ -44,12 +57,18 @@ class App extends Component {
 		}
 	}
 
+	handleClick = (e) => {
+		const value = e.target.getAttribute('data-value');
+		this.handleValue(value);
+	}
+
 	calculate = () => {
 		const input = this.state.queue.join('');
 		if (input.length) {
 			const result = math.eval(input);
 			const formattedResult = math.format(result, { precision: 14 });
 			this.setState({
+				...this.state,
 				queue: [formattedResult]
 			});
 		}
@@ -62,7 +81,7 @@ class App extends Component {
 					<Display input={this.state.queue} />
 					<div className='panel'>
 						<div className='number-container'>
-						<Button size='3x' color="white" onClick={this.handleClick} value='clear' style={{ borderTopLeftRadius: 20 }}/>
+							<Button size='3x' color="white" onClick={this.handleClick} value='clear' style={{ borderTopLeftRadius: 20 }} />
 							{
 								[...Array(9).keys()]
 									.sort((a, b) => b - a)
